eff3e74282f9ec2f0bc564799da95003e1544313
EshanDoes <eshan.shiraz@gmail.com>
2025-11-03 14:48:11 -0500

    Hourly Commit 7.5.5
    Just a tiny commit so I can bring what I did from my laptop to my PC.
    
    Added a few more opposites, made the card colors more consistent, and made the opposites selected each time the card goes down.

--- scripts/cards.gd
+++ scripts/cards.gd
@@ -4,6 +4,7 @@ var leftColor = Color.from_hsv(1,1,1)
 var rightColor = Color.from_hsv(1,1,1)
 
 var addedY = 0
+var targetPosition
 
 var oppositesJSON = "res://other/opposites.json"
 
@@ -15,8 +16,8 @@ func _ready():
 
 # Generate a random color for each half of the card (the code Color.from_hsv(randf(), 1, 1) is taken from Copilot AI Summary)
 func randomColors():
-	leftColor = Color.from_hsv(randf(), 1, 0.8)
-	rightColor = Color.from_hsv(randf(), 1, 0.8)
+	leftColor = Color.from_hsv(round(randf()*32)/32, 1, 0.8)
+	rightColor = Color.from_hsv(round(randf()*32)/32, 1, 0.8)
 	
 	$lefthalf.modulate = leftColor
 	$righthalf.modulate = rightColor
@@ -30,6 +31,7 @@ func _input(event):
 			
 			await get_tree().create_timer(0.01).timeout
 		randomColors()
+		generateOpposites()
 		await get_tree().create_timer(3).timeout
 		for i in 50:
 			addedY = 220 - ease(i/50.0, 0.3) * 220
